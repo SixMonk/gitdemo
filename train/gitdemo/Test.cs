33f083da16826a4fef89a56aa4bb91e47cd3739e
sixmonk <lee_yy0217@163.com>
2017-07-28 07:59:19 +0800

    test

---
+++ train/gitdemo/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitdemo
+{
+    public class Test
+    {
+        public void Test1()
+        {
+            string ss = "hello";
+            string ss2 = "hello";
+            string ss3 = "hello";
+            string ss4 = "hello";
+            string ss5 = "hello";
+            string ss6 = "hello";
+        }
+    }
+}
